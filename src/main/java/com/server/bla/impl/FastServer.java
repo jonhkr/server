6698e2c4158fbca9a2eafb39e697a6a8f16dae31
Jonas Trevisan <jonhkr@gmail.com>
2016-05-08 00:48:22 -0300

    Many changes

--- src/main/java/com/server/bla/impl/FastServer.java
+++ src/main/java/com/server/bla/impl/FastServer.java
@@ -13,9 +13,9 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  * Created by jonhkr on 5/5/16.
@@ -24,7 +24,7 @@ public class FastServer implements Server {
 
     private final ServerOptions options;
     private final Logger logger = LoggerFactory.getLogger(FastServer.class);
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private Node<Worker> currentWorker;
 
     public FastServer(ServerOptions options) {
         this.options = options;
@@ -32,6 +32,8 @@ public class FastServer implements Server {
 
     @Override
     public void listen() throws IOException {
+        setupWorkers(2);
+
         ServerSocketChannel channel = ServerSocketChannel.open();
         Selector selector = Selector.open();
 
@@ -55,58 +57,44 @@ public class FastServer implements Server {
                 iterator.remove();
 
                 if (key.isAcceptable()) {
-                    SocketChannel socketChannel = ((ServerSocketChannel) key.channel()).accept();
-                    logger.debug("Accept connection from: " + socketChannel.getRemoteAddress());
-                    socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                }
+                    SocketChannel sChannel = ((ServerSocketChannel) key.channel()).accept();
 
-                if (key.isReadable()) {
-                    SocketChannel socketChannel = (SocketChannel) key.channel();
-                    Request request = (Request) key.attachment();
+                    Worker worker = currentWorker.item;
+                    currentWorker = currentWorker.next;
 
-                    if (request == null) {
-                        request = new Request(socketChannel);
-                        key.attach(request);
-                    } else {
-                        request.setChannel(socketChannel);
-                    }
-
-                    boolean keepAlive = true;
-
-                    try {
-                        request.readData();
-                    } catch (IOException e) {
-                        keepAlive = false;
-                    }
-
-                    String line;
-                    while((line = request.readLine()) != null) {
-                        logger.debug(line);
-                        if (line.isEmpty()) {
-                            StringBuilder sb = new StringBuilder();
-                            sb.append("HTTP/1.1 204 No content\r\n");
-                            sb.append("Cache-Control: private\r\n");
-                            sb.append("Content-Length: 0\r\n");
-                            sb.append("Date: Sat, 07 May 2016 16:14:47 GMT\r\n\r\n");
-
-                            socketChannel.write(ByteBuffer.wrap(sb.toString().getBytes()));
-                        }
-                    }
-
-                    if (!keepAlive) {
-                        socketChannel.close();
-                    }
+                    worker.accept(sChannel);
                 }
             }
         }
     }
 
+    private void setupWorkers(int n) {
+        Node<Worker> current;
+        Node<Worker> last = null;
+        for(int i = 0; i < n; i++) {
+            Worker worker = new Worker();
+            new Thread(worker).start();
+
+            current = new Node<>(worker, null);
+
+            if (currentWorker == null) {
+                currentWorker = current;
+            } else {
+                last.next = current;
+            }
+
+            last = current;
+        }
+
+        last.next = currentWorker;
+    }
+
     class Request {
 
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer buffer = ByteBuffer.allocate(64*1024);
         SocketChannel channel;
         int mark = 0;
+        int keepAlive = 1;
 
         public Request(SocketChannel channel) {
             this.channel = channel;
@@ -116,7 +104,7 @@ public class FastServer implements Server {
             int byteCount = channel.read(buffer);
 
             if (byteCount == -1) {
-                throw new IOException("EOF");
+                keepAlive = 0;
             }
 
             buffer.flip();
@@ -150,6 +138,107 @@ public class FastServer implements Server {
         public void clear() {
             mark = 0;
             buffer.clear();
+        }
+
+        public void setKeepAlive(int keepAlive) {
+            this.keepAlive = keepAlive;
+        }
+
+        public int getKeepAlive() {
+            return keepAlive;
+        }
+    }
+
+    class Worker implements Runnable {
+        final Selector selector;
+        final Queue<SocketChannel> connected = new ArrayBlockingQueue<>(1000);
+
+        public Worker() {
+            try {
+                selector = Selector.open();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+               loop();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void loop() throws IOException {
+            while (true) {
+                SocketChannel c;
+                while((c = connected.poll()) != null) {
+                    c.configureBlocking(false);
+                    c.register(selector, SelectionKey.OP_READ);
+                }
+
+                if (selector.select() == 0) {
+                    continue;
+                }
+
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectedKeys.iterator();
+
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    iterator.remove();
+
+                    if (key.isReadable()) {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        Request request = (Request) key.attachment();
+
+                        if (request == null) {
+                            request = new Request(socketChannel);
+                            key.attach(request);
+                        } else {
+                            request.setChannel(socketChannel);
+                        }
+
+                        request.readData();
+
+                        String line;
+                        while((line = request.readLine()) != null) {
+
+                            if (line.equals("Connection: close") || line.contains("HTTP/1.0")) {
+                                request.setKeepAlive(0);
+                            }
+
+                            if (line.isEmpty()) {
+                                StringBuilder sb = new StringBuilder();
+                                sb.append("HTTP/1.0 200 Ok\r\n");
+                                sb.append("Content-Length: 0\r\n\r\n");
+
+                                socketChannel.write(ByteBuffer.wrap(sb.toString().getBytes()));
+                            }
+                        }
+
+                        if (request.getKeepAlive() == 0) {
+                            socketChannel.close();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void accept(SocketChannel channel) throws IOException {
+            connected.add(channel);
+            selector.wakeup();
+        }
+    }
+
+    private class Node<E> {
+        E item;
+        Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
         }
     }
 }
